bffd3e485e4093951ee194c1ab7b3779bd426dd6
Michaelhyunookoh <Michaelhyunookoh@gmail.com>
2020-06-15 14:22:01 -0700

    DONE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import List from './List.js';
+import STORE from './store.js';
+import './App.css';
 
-function App() {
+
+function App(prop) {
+  let tempArray = []
+  for (let j = 0; j<STORE.lists.length; j++ ){
+
+  let cardArray = []
+   for(let i=0; i<STORE.lists[j].cardIds.length; i++) {
+     console.log(i,j)
+    let cardId = STORE.lists[j].cardIds[i];
+    cardArray.push(STORE.allCards[cardId])
+  }
+  const tempVariable = <List key = {STORE.lists[j].id} header = {STORE.lists[j].header} cards = {cardArray}/>
+  tempArray.push(tempVariable);
+}
+console.log(tempArray);
   return (
-    <main className='App'>
-      {/* content goes here */}
+    <main className ='App'>
+      <header className='App-header'>
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className='App-List'>
+      {tempArray}
+      </div>
     </main>
   );
 }
-
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,6 +1,19 @@
 import React from 'react';
-export default function list(props) {
+import Card from './Card.js';
+import './List.css';
+ function List(props) {
+     const cards = props.cards.map((card, index) =>
+        <Card key={index} title={card.title} content={card.content} />);
     return (
-        <section
-    )
+        <section className='List'>
+            <header className='List-header'>
+                <h2>{props.header}</h2>
+            </header>
+            <div className='List-cards'>
+                {cards}
+                <button type="button" class="List-add-button">+ Add Random Card</button>
+            </div>
+        </section>
+    );
 }
+export default List
