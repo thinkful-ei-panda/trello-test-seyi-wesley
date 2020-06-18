1a4a24212875012af42d937a5c9da542cf17477f
Wesley Rou <wesleyrou@gmail.com>
2020-06-18 08:09:21 -0700

    App test

--- src/List.test.js
+++ src/List.test.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import List from './List';
 import renderer from 'react-test-renderer';
+import STORE from './store';
 
 // this is the test case
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<List />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
+const props = STORE.lists[0];
+
 it('renders the UI as expected', () => {
-  const tree = renderer
-    .create(<App />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();  
+    const tree = renderer
+        .create(<List {...props}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();  
   });
